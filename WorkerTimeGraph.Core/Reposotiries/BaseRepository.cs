3915ec13232c42e8f86ae87684bb4a0aaabba97f
EnaEd <djaziz12393@gmail.com>
2019-07-13 14:10:13 +0300

    change logic service and repositories

---
+++ WorkerTimeGraph.Core/Reposotiries/BaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+
+using System.Text;
+using WorkerTimeGraph.Core.Interface;
+using WorkerTimeGraph.Core.Model;
+
+namespace WorkerTimeGraph.Core.Reposotiries
+{
+    public class BaseRepository
+    {
+        public BaseRepository()
+        {
+            string applicationFolderPath = System.IO.Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "CanFindLocation");
+            System.IO.Directory.CreateDirectory(applicationFolderPath);
+
+            string databaseFileName = System.IO.Path.Combine(applicationFolderPath, "CanFindLocation.db");
+
+            Database = new SQLiteConnection(databaseFileName);
+            Database.CreateTable<Staff>();
+            Database.CreateTable<TimeWorked>();
+        }
+        public SQLiteConnection Database { get; set; }
+    }
+}
---
+++ WorkerTimeGraph.Core/Reposotiries/StaffRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkerTimeGraph.Core.Interface;
+using WorkerTimeGraph.Core.Model;
+
+namespace WorkerTimeGraph.Core.Reposotiries
+{
+    public class StaffRepository :BaseRepository, ICRUDService<Staff>
+    {
+        public void Create(Staff staff)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Staff Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Staff> GetAll()
+        {
+            return Database.Table<Staff>().ToList();
+        }
+
+        public void Update(Staff staff)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
